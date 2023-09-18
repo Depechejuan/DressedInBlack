e8a3b2f44658ee8c4f2e7ae58f2f61371a97954b
Depechejuan <depechejuan@gmail.com>
2023-09-18 13:21:18 +0200

    Changes on slider

--- dib-react/src/components/Images-slider.jsx
+++ dib-react/src/components/Images-slider.jsx
@@ -10,3 +10,17 @@ export const SliderIMG = [
         {image: "../../public/slider/dib09.jpg"},
         {image: "../../public/slider/dib10-white.png"},
     ];
+
+
+    export const SliderIMG2 = [
+        {image: "../../public/slider/dib01-black-v2.png"},
+        {image: "../../public/slider/dib02-photo-v2.jpg"},
+        {image: "../../public/slider/dib03-endshow-v2.jpg"},
+        {image: "../../public/slider/dib04-band-v2.jpg"},
+        {image: "../../public/slider/dib05-escri-v2.jpg"},
+        {image: "../../public/slider/dib06-dpj-v2.jpg"},
+        {image: "../../public/slider/dib07-javi-v2.jpg"},
+        {image: "../../public/slider/dib08-luis-v2.jpg"},
+        {image: "../../public/slider/dib09-v2.jpg"},
+        {image: "../../public/slider/dib10-white-v2.png"},
+    ]
--- dib-react/src/components/Slider.jsx
+++ dib-react/src/components/Slider.jsx
@@ -1,24 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-import { SliderIMG } from "./Images-slider"
+import { SliderIMG, SliderIMG2 } from "./Images-slider";
 
-function Slider({slides}) {
+function Slider() {
     const [currentImage, setCurrentImage] = useState(0);
-    
+    const [slides, setSlides] = useState(SliderIMG);
 
-    
-    const length = slides.length
+    const length = slides.length;
 
     const nextSlide = () => {
-        setCurrentImage(currentImage === length -1 ? 0 : currentImage + 1);
+        setCurrentImage(currentImage === length - 1 ? 0 : currentImage + 1);
     };
 
     const prevSlide = () => {
-        setCurrentImage(currentImage === 0 ? length -1 : currentImage - 1);
+        setCurrentImage(currentImage === 0 ? length - 1 : currentImage - 1);
     };
 
+    // Detecta el cambio en el ancho de la pantalla usando useEffect
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 900) {
+                setSlides(SliderIMG); // Cambia a SliderIMG2 si el ancho es menor o igual a 900px
+            } else {
+                setSlides(SliderIMG2); // Vuelve a SliderIMG si el ancho es mayor que 900px
+            }
+        };
+
+        // Agrega un event listener para escuchar el cambio de tamaño de la pantalla
+        window.addEventListener("resize", handleResize);
+
+        // Llama a handleResize al cargar la página
+        handleResize();
+
+        // Limpia el event listener cuando el componente se desmonta
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     if (!Array.isArray(slides) || slides.length <= 0) {
-        return null
+        return null;
     }
 
     return (
@@ -26,17 +47,17 @@ function Slider({slides}) {
             <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
             <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
             <div className="slider-content">
-                {SliderIMG.map((slide, index) => {
-                return (
-                    <div
-                    className={index === currentImage ? 'slide active' : 'slide'}
-                    key={index}
-                    >
-                    {index === currentImage && (
-                        <img src={slide.image} alt='travel image' className='image' />
-                    )}
-                    </div>
-                );
+                {slides.map((slide, index) => {
+                    return (
+                        <div
+                            className={index === currentImage ? 'slide active' : 'slide'}
+                            key={index}
+                        >
+                            {index === currentImage && (
+                                <img src={slide.image} alt='travel image' className='image' />
+                            )}
+                        </div>
+                    );
                 })}
             </div>
         </section>
@@ -44,18 +65,3 @@ function Slider({slides}) {
 }
 
 export default Slider;
-
-
-
-    // const images = [
-    //     "../../public/slider/dib01-black.png",
-    //     "../../public/slider/dib02-photo.jpg",
-    //     "../../public/slider/dib03-endshow.jpg",
-    //     "../../public/slider/dib04-band.jpg",
-    //     "../../public/slider/dib05-escri.jpg",
-    //     "../../public/slider/dib06-dpj.jpg",
-    //     "../../public/slider/dib07-javi.jpg",
-    //     "../../public/slider/dib08-luis.jpg",
-    //     "../../public/slider/dib09-murcia.jpg",
-    //     "../../public/slider/dib10-white.png"
-    // ];
