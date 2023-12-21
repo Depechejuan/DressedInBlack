313bdabfe6c79f53bc1feee2c0bd9a927212e0ea
Depechejuan <depechejuan@gmail.com>
2023-12-21 17:28:48 +0100

    changes on modal

--- dib-react/src/components/Modal.jsx
+++ dib-react/src/components/Modal.jsx
@@ -1,4 +1,14 @@
-import Modal from 'react-modal';
+function Modal({link}) {
 
-Modal.setAppElement('#root');
+    return(
+        <div className="modal">
+            <div className="modal-content">
+                <img 
+                    src={link}
+                />
+            </div>
+        </div>
+    )
+}
 
+export default Modal;
