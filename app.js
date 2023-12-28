ce390db74bc7e623e805e513f7176f2b1267bf44
depechejuan <depechejuan@gmail.com>
2023-12-28 10:39:17 +0100

    update CORS

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(
             "http://localhost",
             "https://busy-rose-clam.cyclic.app",
             "https://busy-rose-clam.cyclic.app/users",
+            "https://dressedinblack.netlify.app/",
         ],
     })
 );
